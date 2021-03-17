e6b603f8ebdc6c140d895053bd35fd76b6c271e2
atkntuncer <atkntuncer@gmail.com>
2021-03-17 20:44:44 +0300

    Template

---
+++ CoreConsoleTemplate/DataAccess.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreConsoleTemplate
+{
+    public class DataAccess : IDataAccess
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<DataAccess> _logger;
+
+        public DataAccess(IConfiguration configuration, ILogger<DataAccess> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+        private string GetCS(string name = "ConnectionString")
+        {
+            return _configuration.GetConnectionString("ConnectionString");
+        }
+
+        //methods
+    }
+}
---
+++ CoreConsoleTemplate/IDataAccess.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CoreConsoleTemplate
+{
+    public interface IDataAccess
+    {
+        
+    }
+}
