fbdaba09da3a81181005e9c1ddf411d60a61aff1
atkntuncer <atkntuncer@gmail.com>
2021-03-20 16:36:30 +0300

    Added generic data access methods

--- CoreConsoleTemplate/DataAccess.cs
+++ CoreConsoleTemplate/DataAccess.cs
@@ -16,17 +16,178 @@ namespace CoreConsoleTemplate
     {
         private readonly IConfiguration _configuration;
         private readonly ILogger<DataAccess> _logger;
-
         public DataAccess(IConfiguration configuration, ILogger<DataAccess> logger)
         {
             _configuration = configuration;
             _logger = logger;
         }
-        private string GetCS(string name = "ConnectionString")
+        public IEnumerable<T> Read<T>(string sqlQuery)
         {
-            return _configuration.GetConnectionString("ConnectionString");
+            IEnumerable<T> result=null;
+            try
+            {
+                using (IDbConnection con = new SqlConnection(GetConnectionString()))
+                {
+                    result = con.Query<T>(sqlQuery);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return result;
+        }
+        public IEnumerable<T> ReadWithParamater<T>(string sqlQuery, DynamicParameters param)
+        {
+            IEnumerable<T> result = null;
+            try
+            {
+                using (IDbConnection con = new SqlConnection(GetConnectionString()))
+                {
+                    result = con.Query<T>(sqlQuery, param);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return result;
         }
 
-        //methods
+        public bool Create(string sqlQuery, DynamicParameters param)
+        {
+            int result = -1;
+            try
+            {
+                using (IDbConnection con = new SqlConnection(GetConnectionString()))
+                {
+                    result = con.Execute(sqlQuery, param);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return result > 0;
+        }
+
+        public bool Update(string sqlQuery, DynamicParameters param)
+        {
+            int result = -1;
+            try
+            {
+                using (IDbConnection con = new SqlConnection(GetConnectionString()))
+                {
+                    result = con.Execute(sqlQuery, param);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return result > 0;
+        }
+        public bool Delete(string sqlQuery, DynamicParameters param)
+        {
+            int result = -1;
+            try
+            {
+                using (IDbConnection con = new SqlConnection(GetConnectionString()))
+                {
+                    result = con.Execute(sqlQuery, param);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return result > 0;
+        }
+        public async Task<IEnumerable<T>> ReadAsync<T>(string sqlQuery)
+        {
+            IEnumerable<T> result = null;
+            try
+            {
+                using (IDbConnection con = new SqlConnection(GetConnectionString()))
+                {
+                    result = await con.QueryAsync<T>(sqlQuery);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return result;
+        }
+        public async Task<IEnumerable<T>> ReadWithParamaterAsync<T>(string sqlQuery, DynamicParameters param)
+        {
+            IEnumerable<T> result = null;
+            try
+            {
+                using (IDbConnection con = new SqlConnection(GetConnectionString()))
+                {
+                    result = await con.QueryAsync<T>(sqlQuery, param);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return result;
+        }
+        public async Task<bool> CreateAsync(string sqlQuery, DynamicParameters param)
+        {
+            int result = -1;
+            try
+            {
+                using (IDbConnection con = new SqlConnection(GetConnectionString()))
+                {
+                    result = await con.ExecuteAsync(sqlQuery, param);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return result > 0;
+        }
+
+        public async Task<bool> UpdateAsync(string sqlQuery, DynamicParameters param)
+        {
+            int result = -1;
+            try
+            {
+                using (IDbConnection con = new SqlConnection(GetConnectionString()))
+                {
+                    result = await con.ExecuteAsync(sqlQuery, param);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return result > 0;
+        }
+        public async Task<bool> DeleteAsync(string sqlQuery, DynamicParameters param)
+        {
+            int result = -1;
+            try
+            {
+                using (IDbConnection con = new SqlConnection(GetConnectionString()))
+                {
+                    result = await con.ExecuteAsync(sqlQuery, param);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return result > 0;
+        }
+
+        private string GetConnectionString()
+        {
+            return _configuration.GetConnectionString("");
+        }
     }
 }
--- CoreConsoleTemplate/IDataAccess.cs
+++ CoreConsoleTemplate/IDataAccess.cs
@@ -1,9 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CoreConsoleTemplate
 {
     public interface IDataAccess
     {
-        
+        bool Create(string sqlQuery, DynamicParameters param);
+        Task<bool> CreateAsync(string sqlQuery, DynamicParameters param);
+        bool Delete(string sqlQuery, DynamicParameters param);
+        Task<bool> DeleteAsync(string sqlQuery, DynamicParameters param);
+        IEnumerable<T> Read<T>(string sqlQuery);
+        Task<IEnumerable<T>> ReadAsync<T>(string sqlQuery);
+        IEnumerable<T> ReadWithParamater<T>(string sqlQuery, DynamicParameters param);
+        Task<IEnumerable<T>> ReadWithParamaterAsync<T>(string sqlQuery, DynamicParameters param);
+        bool Update(string sqlQuery, DynamicParameters param);
+        Task<bool> UpdateAsync(string sqlQuery, DynamicParameters param);
     }
 }
